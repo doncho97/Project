ea59525277d225338f73b877655c61f08f12f4a8
doncho97 <don4o97@abv.bg>
2019-03-28 01:44:45 +0200

    Added project

---
+++ C#/VisualStudio2008-WinForms/src/Model/ElipseShape.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace Draw
+{
+    public class ElipseShape : Shape
+    {
+        #region Constructor
+
+        public ElipseShape(RectangleF rect) : base(rect)
+		{
+        }
+
+        public ElipseShape(RectangleShape rectangle) : base(rectangle)
+		{
+        }
+        #endregion
+
+       
+        public override bool Contains(PointF point)
+        {
+            if (base.Contains(point))
+                return true;
+            else
+                return false;
+        }
+
+        public override void DrawSelf(Graphics grfx)
+        {
+            base.DrawSelf(grfx);
+
+            grfx.FillEllipse(new SolidBrush(FillColor), Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+            grfx.DrawEllipse(Pens.Black, Rectangle.X, Rectangle.Y, Rectangle.Width, Rectangle.Height);
+        }
+    }
+}
